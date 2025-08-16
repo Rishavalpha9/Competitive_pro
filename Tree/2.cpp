5f1c44fc12bd1722c900df0314c398932b151a34
Rishav kumar <132774211+Rishavalpha9@users.noreply.github.com>
2025-08-16 13:07:37 +0530

    Update 2.cpp

--- Tree/2.cpp
+++ Tree/2.cpp
@@ -1,1 +1,26 @@
+94. Binary Tree Inorder Traversal
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
 
+if(root ==NULL)
+{
+    return {};
+
+}
+vector<int> rama;
+vector<int> left=inorderTraversal(root->left);
+rama.insert(rama.end(),left.begin(),left.end());
+
+rama.push_back(root->val);
+
+
+vector<int> right=inorderTraversal(root->right);
+rama.insert(rama.end(),right.begin(),right.end());
+
+
+return rama;
+        
+    }
+};
