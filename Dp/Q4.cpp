9e763bbc5c8e79bcaa8ba9254a87ce42946245e7
Rishav kumar <132774211+Rishavalpha9@users.noreply.github.com>
2025-08-16 15:12:58 +0530

    Update Q4.cpp

--- Dp/Q4.cpp
+++ Dp/Q4.cpp
@@ -80,6 +80,44 @@ public:
     }
 };
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  constant space 
   
+class Solution {
+public:
+    int solve(vector<int>& nums, int l, int r) {
+        int prev = 0;
+        int prevprev = 0;
+
+        for (int i = l; i <= r; i++) {
+
+            int skip = prev;
+            int take = nums[i] + prevprev;
+            int temp = max(skip, take);
+
+            prevprev = prev;
+            prev = temp;
+        }
+
+        return prev;
+    }
+
+    int rob(vector<int>& nums) {
+
+        int n = nums.size();
+        if (n == 1) {
+
+            return nums[0];
+        }
+        if (n == 2) {
+            return max(nums[0], nums[1]);
+        }
+
+        int take_first_house = solve(nums, 0, n - 2);
+        int take_second_house = solve(nums, 1, n - 1);
+
+        return max(take_first_house, take_second_house);
+    }
+};
+
 
 
