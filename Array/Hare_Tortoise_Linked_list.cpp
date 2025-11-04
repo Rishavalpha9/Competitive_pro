68913854141499fd63568d1d01cda2c400e739c9
Rishav kumar <132774211+Rishavalpha9@users.noreply.github.com>
2025-11-04 12:28:41 +0530

    Refactor findDuplicate function for clarity

--- Array/Hare_Tortoise_Linked_list.cpp
+++ Array/Hare_Tortoise_Linked_list.cpp
@@ -3,23 +3,23 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int slow = nums[0];
-        int fast = nums[0];
-        
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-        
-        while(slow != fast) {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
+        int slow=nums[0];// pointer starts at index 0
+        int fast= nums[0];
+        slow=nums[slow];
+        fast=nums[nums[fast]];
+
+        while(slow!=fast)
+        {
+
+            slow=nums[slow]; // 1 steps
+            fast=nums[nums[fast]]; // 2 steps
         }
-        
-        slow = nums[0];
-        while(slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
+        slow=nums[0];
+        while(slow!=fast)
+        {
+            slow=nums[slow]; // 1 step 
+            fast=nums[fast];  // both 1 step now
         }
-        
         return slow;
     }
 };
